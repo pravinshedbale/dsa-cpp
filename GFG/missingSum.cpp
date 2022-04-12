38b621ce611607f39e12b92bc282a88d821c437d
pravinshedbale <pravin.shedbale@outlook.com>
2022-04-12 18:17:51 +0530

    missing sum problem

---
+++ GFG/missingSum.cpp
@@ -0,0 +1,11 @@
+class Solution{
+  public:
+    int MissingNumber(vector<int>& array, int n) {
+        int sum = 0;
+        int targetSum = n*(n+1)/2;
+        for(int n:array) {
+            sum+=n;
+        }
+        return targetSum-sum;
+    }
+};
