1b10d714ee535af0f95248a61ac80fafb44760b2
pravinshedbale <pravin.shedbale@outlook.com>
2022-04-21 22:03:14 +0530

    Recursion Program

--- GFG/1. Math/1. Missing Sum/missingSum.cpp
+++ GFG/1. Math/1. Missing Sum/missingSum.cpp
@@ -3,6 +3,7 @@ class Solution{
     int MissingNumber(vector<int>& array, int n) {
         int sum = 0;
         int targetSum = n*(n+1)/2;
+        
         for(int n:array) {
             sum+=n;
         }
